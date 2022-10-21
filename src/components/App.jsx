bf9c6adfb48592cc5e86f0c2ecd601154a230192
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-10-21 22:25:04 +0300

    some test router function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import { About } from '../pages/About';
+import { Home } from '../pages/Home';
+import { ProductDetails } from '../pages/ProductDetails';
+import { Products } from '../pages/Products';
+import { Mission } from './Mission';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
+import { SharedLayout } from './SharedLayout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
