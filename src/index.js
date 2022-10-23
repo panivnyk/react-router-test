7c2a1aaa9f8c274301af547ff290302b45f18511
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-10-23 11:14:38 +0300

    add pages and function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { App } from './components/App';
+import 'modern-normalize';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
